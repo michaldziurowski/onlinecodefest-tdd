786d07345f18b62209ddca63899be49988d8d1e5
aja <aja@P-AJA>
2016-12-10 11:53:41 +0100

    king

---
+++ ChessMastaEngine.Obojetnie/ChessMastaEngine.Objojetnie.Tests/Chess_King_Test.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections.Generic;
+using ChessMastaEngine.Obojetnie;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessMastaEngine.Objojetnie.Tests
+{
+    [TestClass]
+    public class Chess_King_Test
+    {
+        private PieceOnChessBoard _myPiece;
+        [TestInitialize]
+        public void InitTest()
+        {
+            _myPiece = new PieceOnChessBoard
+            {
+                Position = new Position("d4"),
+                Color = Color.White
+            };
+        }
+
+        [TestMethod]
+        public void ChessKing_VerticallyByOneFieldUp_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("d5");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_VerticallyByTwoFieldUp_Incorrect()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("d6");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_VerticallyByTwoFieldDown_Incorrect()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("d2");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_VerticallyByOneFieldDown_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("d3");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_HorizontallyByOneFieldRight_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("e4");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_HorizontallyByOneFieldLeft_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("c4");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_HorizontallyByTwoFieldsLeft_Incorrect()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("b4");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_DiangonalyByOneFieldLeftUp_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("c5");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_DiangonalyByOneFieldLeftDown_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("c3");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_DiangonalyByOneFieldRightDown_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("e3");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_DiangonalyByOneFieldRightUp_Correct()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("e5");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_DiangonalyByTwoFieldsRightUp_Incorrect()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("e6");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_DiangonalyByThreeFieldsRightDown_Incorrect()
+        {
+            var king = new King(_myPiece);
+            bool result = king.MoveTo("f6");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_DontChangePosition_Correct()
+        {
+            var myPiece = new PieceOnChessBoard
+            {
+                Position = new Position("d4"),
+                Color = Color.White
+            };
+            var king = new King(myPiece);
+            bool result = king.MoveTo("d4");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_FieldUpTakenByPieceFromTheSameColor_Inorrect()
+        {
+            List<PieceOnChessBoard> piecesOnBoard = new List<PieceOnChessBoard>
+            {
+                new PieceOnChessBoard
+                {
+                    Position =new Position("e5"),
+                    Color = Color.White
+                }
+            };
+
+            var king = new King(_myPiece, piecesOnBoard);
+            bool result = king.MoveTo("e5");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_FieldUpTakenByPieceFromTheOppositeColor_Correct()
+        {
+            List<PieceOnChessBoard> piecesOnBoard = new List<PieceOnChessBoard>
+            {
+                new PieceOnChessBoard
+                {
+                    Position =new Position("e5"),
+                    Color = Color.Black
+                }
+            };
+
+            var king = new King(_myPiece, piecesOnBoard);
+            bool result = king.MoveTo("e5");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ChessKing_TwoFieldUpTakenByKingOfTheOppositeColor_Incorrect()
+        {
+            List<PieceOnChessBoard> piecesOnBoard = new List<PieceOnChessBoard>
+            {
+                new PieceOnChessBoard
+                {
+                    Position =new Position("d6"),
+                    Color = Color.Black,
+                    IsKing = true
+                }
+            };
+
+            var king = new King(_myPiece, piecesOnBoard);
+            bool result = king.MoveTo("d5");
+
+            Assert.IsFalse(result);
+        }
+    }
+}
+
---
+++ ChessMastaEngine.Obojetnie/ChessMastaEngine.Obojetnie/King.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMastaEngine.Obojetnie
+{
+    public class King
+    {
+        private readonly PieceOnChessBoard _myPiece;
+        private readonly List<PieceOnChessBoard> _takenFields;
+
+        public King(PieceOnChessBoard myPiece) : this(myPiece, new List<PieceOnChessBoard>()){}
+
+        public King(PieceOnChessBoard myPiece, List<PieceOnChessBoard> takenFields)
+        {
+            _myPiece = myPiece;
+            _takenFields = takenFields;
+        }
+
+        public bool MoveTo(string position)
+        {
+            var newPosition = new Position(position);
+            int horizontalDifference = Math.Abs((newPosition.Y - _myPiece.Position.Y));
+            int verticalDifference = Math.Abs(newPosition.X - _myPiece.Position.X);
+
+            return (horizontalDifference == 0 || horizontalDifference == 1)
+                   && (verticalDifference == 0 || verticalDifference == 1)
+                   && !(verticalDifference == 0 && horizontalDifference == 0)
+                   && !_takenFields.Any(p => p.Color == _myPiece.Color && p.Position.X == newPosition.X && p.Position.Y == newPosition.Y)
+                   && CheckIfMoveNearOtherKing(newPosition);
+        }
+
+        private bool CheckIfMoveNearOtherKing(Position start)
+        {
+            var otherKing = _takenFields.FirstOrDefault(p => p.IsKing && p.Color != _myPiece.Color);
+
+            if (otherKing == null)
+            {
+                return true;
+            }
+
+            int horizontalDifference = Math.Abs((otherKing.Position.Y - start.Y));
+            int verticalDifference = Math.Abs(otherKing.Position.X - start.X);
+
+            return !((horizontalDifference == 0 || horizontalDifference == 1)
+                     && (verticalDifference == 0 || verticalDifference == 1));
+        }
+    }
+
+    public enum Color
+    {
+        White,
+        Black
+    }
+
+    public class PieceOnChessBoard
+    {
+        public bool IsKing { get; set; }
+        public Color Color { get; set; }
+        public Position Position { get; set; }
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(string position)
+        {
+            X = position.Substring(0, 1).ToCharArray()[0];
+            Y = int.Parse(position.Substring(1, 1));
+        }
+    }
+}
