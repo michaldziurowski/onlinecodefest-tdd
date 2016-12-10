dc281d6f2c22a27c1cfbd1b0afda93207a92f4db
Maciej Jędrzejewski <mac.jedrzejewski@gmail.com>
2016-12-10 10:06:23 +0100

    Initial commit for swiss solution

---
+++ CSharpClientSwiss/CSharpClientSwiss/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpClientSwiss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage:\nChessMasta.exe serverUrl");
+            }
+            else
+            {
+                Console.WriteLine("Let the game begin - swiss");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
